96192834a1b0a050c0e16bd5d900fefd4706c5f4
Vu Nguyen <vu@simble.io>
2018-04-27 09:54:33 +0700

    working with route laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h2>About this site</h2>
+There are over <?php echo $number_of_cats; ?> cats on this site
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('cats/{id}', function ($id) {
+    return 'Cat ' . $id;
+});
+
+Route::get('cats/{id}/{name}', function ($id, $name) {
+    return 'Cat ' . $id . ' name is: ' . $name ;
+});
+
+Route::get('/', function() {
+   return redirect('cats');
+});
+
+Route::get('cats', function() {
+   return 'All cats';
+});
+
+Route::get('about', function() {
+   $number_of_cats = 9000;
+   return view('about', compact('number_of_cats'));
 });
