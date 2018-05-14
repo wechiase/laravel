e98d235e5f7768dc0bf3c623a6150cc953d4e128
Vu Nguyen <vu@simble.io>
2018-05-14 23:10:20 +0700

    how to work with layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,7 @@
-<h2>About this site</h2>
-There are over <?php echo $number_of_cats; ?> cats on this site
+@extends('layouts.master')
+@section('header')
+    <h2>About this site</h2>
+@stop
+@section('content')
+    There are over <?php echo $number_of_cats; ?> cats on this site
+@stop
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Larave wechiase.com</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+  </head>
+  <body>
+    <div class="container">
+      <div class="page-header">
+        @yield('header')
+      </div>
+      @if (Session::has('success'))
+        <div class="alert alert-success">
+            {{ Session::get('success') }}
+        </div>
+      @endif
+      @yield('content')
+    </div>
+  </body>
+</html>
